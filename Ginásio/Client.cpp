b9682ce862f8f501794708509283b9db57cb9817
Duarte-Frazao <duartemarquesmano@gmail.com>
2017-10-15 00:09:43 +0100

    Gym constructor

--- Ginásio/Client.cpp
+++ Ginásio/Client.cpp
@@ -8,7 +8,12 @@
 #include "Gym.h"
 #include <iostream>
 
-Client::Client(): insideGym(false)  {
+using namespace std;
+
+Client::Client(string clientName, int program, int clientAge, PersonalTrainer *PT):
+	insideGym(false), paymentsUpToDate(true), numLatePayments(0), enrolledProgram(program),
+	age(clientAge), responsiblePT(PT){
+	//To-do initialize numDays remaining based on the program chosen
 }
 
 Client::~Client() {
--- Ginásio/Client.h
+++ Ginásio/Client.h
@@ -8,15 +8,20 @@
 #ifndef CLIENT_H_
 #define CLIENT_H_
 
-
+#include <string>
 #include "PersonalTrainer.h"
 class PersonalTrainer;
+using namespace std;
 
 class Client {
 public:
-	Client();
+
+	Client(std::string clientName,int program, int clientAge, PersonalTrainer *PT);
 	~Client();
 private:
+
+	std::string name;
+	int age;
 	bool insideGym;
 	bool paymentsUpToDate;
 	int numLatePayments;

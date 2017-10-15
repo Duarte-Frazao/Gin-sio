35fa6338028d4e42c33c917eb0eadd277ecd2020
Duarte-Frazao <duartemarquesmano@gmail.com>
2017-10-15 01:14:53 +0100

    Part of the basic class of Client

--- Ginásio/Client.h
+++ Ginásio/Client.h
@@ -13,11 +13,36 @@
 class PersonalTrainer;
 using namespace std;
 
+class EntranceError {
+	string reason;
+public:
+	EntranceError(string rz);
+	string getReason() const;
+	friend ostream & operator << (ostream &out, EntranceError &error);
+};
+
 class Client {
 public:
 
 	Client(std::string clientName,int program, int clientAge, PersonalTrainer *PT);
 	~Client();
+	string getName() const;
+	int getAge() const;
+	bool getLocation() const;
+	bool getPaymentStatus() const;
+	int getNumLatePayments() const;
+	int getProgram() const;
+	int getDaysRemaining() const;
+	const PersonalTrainer *getPT() const;
+	void setProgram(int newProgram);
+	void setPT(PersonalTrainer *PT);
+	void changeLocation();
+
+	//void updatePayments() const;
+	//void changeProgram() const;
+	//void changePT() const;
+	//void editBasicSettings() const; client, name age
+
 private:
 
 	std::string name;
